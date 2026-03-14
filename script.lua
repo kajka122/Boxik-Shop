9edb11405ff45ae94841e25e6f43bab53ca56af6
kajka122 <kajabal230700@gmail.com>
2026-03-14 22:12:42 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/twojanazwa/roblox-script/main/script.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kajka122/Boxik-Shop/refs/heads/main/script.lua))()
